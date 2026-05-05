6727801c1f3cb690c0c31c8a741725ea51dc0539
ruslan <aleksander.ruslan@gmail.com>
2026-05-05 23:25:27 +0300

    Auto-create superuser on deploy

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
-#!/usr/bin/env bash 
-pip install -r requirements.txt 
-python manage.py collectstatic --noinput 
-python manage.py migrate 
+#!/usr/bin/env bash
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
+python -c "import django; import os; os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings'); django.setup(); from django.contrib.auth.models import User; User.objects.create_superuser('flexadmin', '', 'Flexgram2026!') if not User.objects.filter(username='flexadmin').exists() else print('admin exists')"
